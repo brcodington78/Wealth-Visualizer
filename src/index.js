1d508ac2a2bdf905934e71f0822281c4895a647f
brcodington78 <brcodington@gmail.com>
2021-08-19 11:02:25 -0700

    made a new circle packing map with billionaire data

--- src/index.js
+++ src/index.js
@@ -18,6 +18,7 @@ let data = billData
 let testData1 = sortByIndustry(billData)
 let testData2 = indObjDataFormatter(testData1)
 
+console.log('billData',billData)
 console.log('industries before conversion', sortByIndustry(billData))
 console.log('testData2', testData2)
 
@@ -67,39 +68,6 @@ d3.json('https://cdn.jsdelivr.net/npm/world-atlas@2/countries-110m.json').then(d
 			.attr('d', pathGenerator)
 			
 })
-
-// function makePieGraph() {
-// 	let svg2 = d3.select('.industry-pie-container');
-// 	let width = svg2.attr('width');
-// 	let height = svg2.attr('height');
-// 	let radius = Math.min(width, height) / 2;
-
-// 	let data = sortByIndustry(billData);
-// 	console.log('data', data)
-
-// 	let g = svg2.append('g').attr('transform', 'translate('+ width / 2 + ',' + height / 2 + ')');
-// 	let color = d3.scaleOrdinal().domain(data.map(d => d.name))
-//     .range(d3.quantize(t => d3.interpolateSpectral(t * 0.8 + 0.1), data.length).reverse());
-
-// 	let pie = d3.pie();
-
-// 	let arc = d3.arc()
-// 				.innerRadius(0)
-// 				.outerRadius(radius);
-
-// 	let arcs = g.selectAll('arc')
-// 				.data(pie(data))
-// 				.enter().append('g')
-// 				.attr('class','arc')
-// 	arcs.append('path')
-// 		.attr("fill", d => color(d.data.name))
-// 		.attr('d', arc);
-
-// 	console.log('color', color())
-// }
-
-// makePieGraph()
-
 
 
 
@@ -162,62 +130,169 @@ d3.json('https://cdn.jsdelivr.net/npm/world-atlas@2/countries-110m.json').then(d
 // makePieChart3()
 
 
+//makes a circle packing graph using industry networth data 
+//need to make it more general purpose and able to take in whatever industry networth data
 
-const makeBubblesPlz = () => {
-	const width = 460;
-	const height = 460;
+// const makeIndustryBubblesPlz = () => {
+// 	const width = 460;
+// 	const height = 460;
 
-	const svg2 = d3.select('.industry-pie-container')
-		.append('svg')
-			.attr('width', width)
-			.attr('height', height)
+// 	const svg2 = d3.select('.industry-pie-container')
+// 		.append('svg')
+// 			.attr('width', width)
+// 			.attr('height', height)
 
 	
-	const data = testData2;
+// 	const data = testData2;
 
-	let color = d3.scaleOrdinal()
-		.domain(allIndustries)
-		.range(d3.schemeSet1);
+// 	let color = d3.scaleOrdinal()
+// 		.domain(allIndustries)
+// 		.range(d3.schemeSet1);
+
+// 	const size = d3.scaleLinear()
+// 		.domain([0,1800])
+// 		.range([7,55])
+
+// 	const Tooltip = d3.select('.industry-pie-container')
+// 		.append('div')
+// 		.style('opacity', 0)
+// 		.attr('class', 'tooltip')
+// 		.style('background-color', 'white')
+// 		.style("border", "solid")
+// 		.style("border-width", "2px")
+// 		.style("border-radius", "5px")
+// 		.style("padding", "5px")
+
+// 	let mouseover = function(event, d) {
+// 		Tooltip
+// 			.style("opacity", 1)
+// 	}
+
+	
+// 	let mousemove = function(event, d) {
+// 		console.log('event', event)
+// 		console.log('mousemove hitting')
+// 		console.log('mousemove d', d)
+// 		const [x, y] = d3.pointer(event);
+// 		Tooltip
+// 		  	.html('<u>' + d.industry + '</u>' + "<br>" + `${d.totalWorth}` + " dollars in billions" )
+// 			.style('position', 'relative')
+// 			.style('z-index', 100)
+// 			.style("left", 230 + "px")
+//       		.style("top", 250 + "px")
+// 		console.log(Tooltip)
+// 	}
+
+// 	let mouseleave = function(event, d) {
+// 		Tooltip
+// 		  .style("opacity", 0)
+// 	}
+
+
+// 	let node = svg2.append("g")
+//     .selectAll("circle")
+//     .data(data)
+//     .join("circle")
+//       .attr("class", "node")
+//       .attr("r", d => size(d.totalWorth))
+//       .attr("cx", width / 2)
+//       .attr("cy", height / 2)
+//       .style("fill", d => color(d.industry))
+//       .style("fill-opacity", 0.8)
+//       .attr("stroke", "black")
+//       .style("stroke-width", 1)
+//       .on("mouseover", mouseover) // What to do when hovered
+//       .on("mousemove", mousemove)
+//       .on("mouseleave", mouseleave)
+//       .call(d3.drag() // call specific function when circle is dragged
+//            .on("start", dragstarted)
+//            .on("drag", dragged)
+//            .on("end", dragended));
+
+
+// 	const simulation = d3.forceSimulation()
+// 	.force("center", d3.forceCenter().x(width / 2).y(height / 2)) // Attraction to the center of the svg area
+// 	.force("charge", d3.forceManyBody().strength(.1)) // Nodes are attracted one each other of value is > 0
+// 	.force("collide", d3.forceCollide().strength(.2).radius(function(d){ return (size(d.totalWorth)+3) }).iterations(1)) // Force that avoids circle overlapping
+
+// 	//changed from v4 to v6
+// 	simulation
+//       .nodes(data)
+//       .on("tick", function(d){
+//         node
+//             .attr("cx", d => d.x)
+//             .attr("cy", d => d.y)
+//       });
+
+// 	  function dragstarted(event, d) {
+// 		if (!event.active) simulation.alphaTarget(.03).restart();
+// 		d.fx = d.x;
+// 		d.fy = d.y;
+// 	  }
+// 	  function dragged(event, d) {
+// 		d.fx = event.x;
+// 		d.fy = event.y;
+// 	  }
+// 	  function dragended(event, d) {
+// 		if (!event.active) simulation.alphaTarget(.03);
+// 		d.fx = null;
+// 		d.fy = null;
+// 	  }
+
+// }
+
+// makeIndustryBubblesPlz()
+
+const billionaireBubbles = (data) => {
+	const width = 500;
+	const height = 500;
+
+	const svg = d3.select('.billionaire-circle-container')
+				.append('svg')
+				.attr('width', height)
+				.attr('height', height)
+	
+	const color = d3.scaleOrdinal()
+			.domain(allIndustries)
+			.range(d3.schemeSet1);
 
 	const size = d3.scaleLinear()
-		.domain([0,1800])
-		.range([7,55])
+		.domain([0, 190])
+		.range([1,50]) 
 
-	let Tooltip = d3.select('.indsutry-pie-container')
-		.append('div')
-		.style('opacity', 0)
-		.attr('class', 'tooltip')
-		.style('background-color', 'white')
+	const Tooltip = d3.select(".billionaire-circle-container")
+		.append("div")
+		.style("opacity", 0)
+		.attr("class", "tooltip")
+		.style("background-color", "white")
 		.style("border", "solid")
 		.style("border-width", "2px")
 		.style("border-radius", "5px")
 		.style("padding", "5px")
-
-	let mouseover = function(event, d) {
+	
+	const mouseover = function(event, d) {
 		Tooltip
 			.style("opacity", 1)
 	}
 
-	//might have to change te d.totalWorth to a string
-	let mousemove = function(event, d) {
+	const mousemove = function(event, d) {
 		Tooltip
-		  .html('<u>' + d.industry + '</u>' + "<br>" + `${d.totalWorth}` + "dollars in billions")
-		  .style("left", (event.x/2+20) + "px")
-		  .style("top", (event.y/2-30) + "px")
+			.html('<u>' + d.name + '</u>' + "<br>" + d.netWorth + " billion dollars" + "<br>" + d.industry + " industry")
+			.style("left", (event.x/2+20) + "px")
+			.style("top", (event.y/2-30) + "px")
 	}
-
+	
 	let mouseleave = function(event, d) {
 		Tooltip
-		  .style("opacity", 0)
+			.style("opacity", 0)
 	}
 
-
-	let node = svg2.append("g")
+	let node = svg.append("g")
     .selectAll("circle")
     .data(data)
     .join("circle")
       .attr("class", "node")
-      .attr("r", d => size(d.totalWorth))
+      .attr("r", d => size(d.netWorth))
       .attr("cx", width / 2)
       .attr("cy", height / 2)
       .style("fill", d => color(d.industry))
@@ -232,43 +307,41 @@ const makeBubblesPlz = () => {
            .on("drag", dragged)
            .on("end", dragended));
 
-
-	const simulation = d3.forceSimulation()
-	.force("center", d3.forceCenter().x(width / 2).y(height / 2)) // Attraction to the center of the svg area
-	.force("charge", d3.forceManyBody().strength(.1)) // Nodes are attracted one each other of value is > 0
-	.force("collide", d3.forceCollide().strength(.2).radius(function(d){ return (size(d.totalWorth)+3) }).iterations(1)) // Force that avoids circle overlapping
-
-	//changed from v4 to v6
-	simulation
-      .nodes(data)
-      .on("tick", function(d){
-        node
-            .attr("cx", d => d.x)
-            .attr("cy", d => d.y)
-      });
-
-	  function dragstarted(event, d) {
-		if (!event.active) simulation.alphaTarget(.03).restart();
-		d.fx = d.x;
-		d.fy = d.y;
-	  }
-	  function dragged(event, d) {
-		d.fx = event.x;
-		d.fy = event.y;
-	  }
-	  function dragended(event, d) {
-		if (!event.active) simulation.alphaTarget(.03);
-		d.fx = null;
-		d.fy = null;
-	  }
-
+		   const simulation = d3.forceSimulation()
+		   .force("center", d3.forceCenter().x(width / 2).y(height / 2)) // Attraction to the center of the svg area
+		   .force("charge", d3.forceManyBody().strength(.1)) // Nodes are attracted one each other of value is > 0
+		   .force("collide", d3.forceCollide().strength(.2).radius(function(d){ return (size(d.netWorth)+3) }).iterations(1)) // Force that avoids circle overlapping
+	 
+	   // Apply these forces to the nodes and update their positions.
+	   // Once the force algorithm is happy with positions ('alpha' value is low enough), simulations will stop.
+	   simulation
+		   .nodes(data)
+		   .on("tick", function(d){
+			 node
+				 .attr("cx", d => d.x)
+				 .attr("cy", d => d.y)
+		   });
+	 
+	   // What happens when a circle is dragged?
+	   function dragstarted(event, d) {
+		 if (!event.active) simulation.alphaTarget(.03).restart();
+		 d.fx = d.x;
+		 d.fy = d.y;
+	   }
+	   function dragged(event, d) {
+		 d.fx = event.x;
+		 d.fy = event.y;
+	   }
+	   function dragended(event, d) {
+		 if (!event.active) simulation.alphaTarget(.03);
+		 d.fx = null;
+		 d.fy = null;
+	   }
 }
 
-makeBubblesPlz()
 
 
-
-
+billionaireBubbles(billData)
 	
 	
 
